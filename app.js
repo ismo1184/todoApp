93f0ccf16f6dc618ca4ba940895f0a8a7f382cf5
Ismail Mohamed <ismo@hotmail.dk>
2023-05-02 17:59:20 +0200

    added filter functionality

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const todos = [];
+let filteredTodos = todos;
+let filterType = "all";
 
 const newTodo = document.querySelector("#newTodo");
 const todoContainer = document.querySelector("#todoContainer");
@@ -26,32 +28,110 @@ function createTodoObject(text) {
     ID: Date.now(),
   };
 }
+function getFilteredTodos(filterType) {
+  if (filterType === "active") {
+    return todos.filter(todo => !todo.checked);
+  } else if (filterType === "completed") {
+    return todos.filter(todo => todo.checked);
+  } else {
+    return todos;
+  }
+}
 
 function addTodoItem(todo) {
   todos.push(todo);
-  renderTodoItem(todo);
+  renderTodoItems(getFilteredTodos());
   newTodo.value = "";
   newTodo.focus();
 }
 
-function renderTodoItem(todo) {
-  const todoItem = document.createElement("li");
-  const todoSpan = document.createElement("span");
-  todoSpan.textContent = todo.text;
-  todoSpan.classList.add("todoText");
-  todoItem.append(todoSpan);
-  todoItem.setAttribute("data-key", `${todo.ID}`);
-  todoItem.classList.add("todoItem");
-  todoContainer.append(todoItem);
-  const todoIcon = document.createElement("span");
-  todoIcon.classList.add("todoIcon");
-  todoItem.prepend(todoIcon);
-  const deleteSVG = document.createElement("img");
-  deleteSVG.src = "images/icon-cross.svg";
-  deleteSVG.classList.add("delete");
-  todoItem.append(deleteSVG);
+// function renderTodoItems(todoArray) {
+//   todoContainer.innerHTML = "";
+//   todoArray.forEach(todo => {
+//     const todoItem = document.createElement("li");
+//     const todoSpan = document.createElement("span");
+//     todoSpan.textContent = todo.text;
+//     todoSpan.classList.add("todoText");
+//     todoItem.append(todoSpan);
+//     todoItem.setAttribute("data-key", `${todo.ID}`);
+//     todoItem.classList.add("todoItem");
+
+//     // Add the todoIcon span to every todoItem element
+//     const todoIcon = document.createElement("span");
+//     todoIcon.classList.add("todoIcon");
+//     todoItem.prepend(todoIcon);
+
+//     if (todo.checked) {
+//       todoItem.classList.add("checkedIcon", "checkedTodo");
+//       const checkedIcon = document.createElement("span");
+//       checkedIcon.classList.add("checkedIcon");
+//       todoItem.prepend(checkedIcon);
+//     }
+
+//     const deleteSVG = document.createElement("img");
+//     deleteSVG.src = "images/icon-cross.svg";
+//     deleteSVG.classList.add("delete");
+//     todoItem.append(deleteSVG);
+//     todoContainer.append(todoItem);
+
+//     const checkedIcon = todoItem.querySelector(".checkedIcon");
+//     if (checkedIcon) {
+//       checkedIcon.classList.toggle("checkedIcon", todo.checked);
+//     }
+//   });
+// }
+
+function renderTodoItems(todoArray) {
+  todoContainer.innerHTML = "";
+  todoArray.forEach(todo => {
+    const todoItem = document.createElement("li");
+    const todoSpan = document.createElement("span");
+    todoSpan.textContent = todo.text;
+    todoSpan.classList.add("todoText");
+    todoItem.append(todoSpan);
+    todoItem.setAttribute("data-key", `${todo.ID}`);
+    todoItem.classList.add("todoItem");
+
+    // Add the todoIcon span to every todoItem element
+    const todoIcon = document.createElement("span");
+    todoIcon.classList.add("todoIcon");
+    todoItem.prepend(todoIcon);
+
+    if (todo.checked) {
+      todoIcon.classList.add("checkedIcon");
+      todoItem.classList.add("checkedTodo");
+    }
+
+    const deleteSVG = document.createElement("img");
+    deleteSVG.src = "images/icon-cross.svg";
+    deleteSVG.classList.add("delete");
+    todoItem.append(deleteSVG);
+    todoContainer.append(todoItem);
+  });
 }
 
+const completedTodos = document.querySelector(".completedTodos");
+
+completedTodos.addEventListener("click", () => {
+  filterType = "completed";
+  const filteredTodos = getFilteredTodos(filterType);
+  renderTodoItems(filteredTodos);
+});
+
+const activeTodos = document.querySelector(".activeTodos");
+
+activeTodos.addEventListener("click", () => {
+  filterType = "active";
+  const filteredTodos = getFilteredTodos(filterType);
+  renderTodoItems(filteredTodos);
+});
+
+const allTodos = document.querySelector(".allTodos");
+
+allTodos.addEventListener("click", () => {
+  filterType = "all";
+  renderTodoItems(todos);
+});
 function deleteTodoItem(ID) {
   const todoID = todos.map(todo => todo.ID);
   const todoIndex = todoID.indexOf(Number(ID));
@@ -67,13 +147,21 @@ todoContainer.addEventListener("click", e => {
   }
 });
 
-function checkTodoItem(ID) {
-  const todoID = todos.map(todo => todo.ID);
-  const todoIndex = todoID.indexOf(Number(ID));
-  if (todos[todoIndex].checked === true) {
-    todos[todoIndex].checked = false;
+function checkTodoItem(ID, filterType) {
+  const todo = getFilteredTodos(filterType).find(
+    todo => todo.ID === Number(ID)
+  );
+  todo.checked = !todo.checked;
+
+  const todoItem = document.querySelector(`[data-key="${ID}"]`);
+  const todoIcon = todoItem.querySelector(".todoIcon");
+
+  if (todo.checked) {
+    todoIcon.classList.add("checkedIcon");
+    todoItem.classList.add("checkedTodo");
   } else {
-    todos[todoIndex].checked = true;
+    todoIcon.classList.remove("checkedIcon");
+    todoItem.classList.remove("checkedTodo");
   }
 }
 
@@ -81,19 +169,5 @@ todoContainer.addEventListener("click", e => {
   if (e.target.classList.contains("todoIcon")) {
     const ID = e.target.parentElement.dataset.key;
     checkTodoItem(ID);
-    const todoItem = document.querySelector(`[data-key= "${ID}"]`);
-    todoItem.classList.toggle("checkedTodo");
-    const checkedIcon = todoItem.querySelector("span");
-    checkedIcon.classList.toggle("checkedIcon");
   }
-});
-
-const completedTodos = document.querySelector(".completedTodos");
-
-completedTodos.addEventListener("click", () => {
-  // Filter the todos array to only include the completed todos
-  const completed = todos.filter(todo => todo.checked);
-
-  // Render the completed todos in the container
-  renderTodos(completed);
 });
